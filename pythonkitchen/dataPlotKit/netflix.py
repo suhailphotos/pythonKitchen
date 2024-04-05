ece9226f4475ce69c301b54c45e6cb8b8790a0cd
Suhail <suhailece@gmail.com>
2024-04-05 15:37:42 -0700

    update neflix.py

--- pythonkitchen/dataPlotKit/netflix.py
+++ pythonkitchen/dataPlotKit/netflix.py
@@ -1,4 +1,9 @@
+import pandas as pd
+
 class Netflix:
-    def __init__(self):
-        print('this is working')
+    def __init__(self, netlix_dataset: str, other_dataset: str):
+        self.netflix_data = pd.read_csv(netlix_dataset)
+        self.other_data = pd.read_csv(other_dataset)
+        self.combined_data = pd.merge(self.netflix_data, self.other_data, on='title', how='inner')
+
 
