9e16aa734328ef5abb63a47eecdd821ea72542fb
Suhail <suhailece@gmail.com>
2024-04-06 13:52:08 -0700

    update netflix.py

--- pythonkitchen/dataPlotKit/netflix.py
+++ pythonkitchen/dataPlotKit/netflix.py
@@ -6,6 +6,15 @@ class Netflix:
         self.netflix_data = pd.read_csv(netlix_dataset)
         self.other_data = pd.read_csv(other_dataset)
         self.combined_data = pd.merge(self.netflix_data, self.other_data, on='title', how='inner')
+        print(f'Netflix data size: {len(self.netflix_data)}')
+        print(f'IMDb data size: {len(self.other_data)}')
+        self._find_mismaches(threshold=90)
+
+    def _find_mismaches(self, threshold=90):
+        mismatched_titles = []
+        unmatched_data = self.other_data[~self.other_data['title'].isin(self.combined_data['title'])]
+        print(f'Unmatched_titles: {len(unmatched_data)}')
+
 
 
 if __name__ == "__main__":
@@ -13,10 +22,7 @@ if __name__ == "__main__":
     netflix_path = f'{project_root}/data/netflix_titles.csv'
     tmdb_path = f'{project_root}/data/TMDb_updated.CSV'
     n = Netflix(netflix_path, tmdb_path)
-    print(len(n.netflix_data))
-    print(len(n.other_data))
-    print(len(n.combined_data))
-   
+       
   
     
 
