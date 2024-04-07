267887d1ca6bddd2ee360d81102a60e7c0726ce2
Suhail <suhailece@gmail.com>
2024-04-07 11:57:45 -0700

    update netflix.py

--- pythonkitchen/dataPlotKit/netflix.py
+++ pythonkitchen/dataPlotKit/netflix.py
@@ -1,20 +1,30 @@
+from fuzzywuzzy import fuzz
+from fuzzywuzzy import process
 import pandas as pd
 import os
 
 class Netflix:
-    def __init__(self, netlix_dataset: str, other_dataset: str):
-        self.netflix_data = pd.read_csv(netlix_dataset)
+    def __init__(self, netflix_dataset: str, other_dataset: str):
+        self.netflix_data = pd.read_csv(netflix_dataset)
         self.other_data = pd.read_csv(other_dataset)
         self.combined_data = pd.merge(self.netflix_data, self.other_data, on='title', how='inner')
-        print(f'Netflix data size: {len(self.netflix_data)}')
-        print(f'IMDb data size: {len(self.other_data)}')
-        self._find_mismaches(threshold=90)
+        self.mismatches = self._find_mismatches(test_mode=True)
 
-    def _find_mismaches(self, threshold=90):
+    def _find_mismatches(self, threshold=3, test_mode=False, max_iterations=3000):
         mismatched_titles = []
         unmatched_data = self.other_data[~self.other_data['title'].isin(self.combined_data['title'])]
-        print(f'Unmatched_titles: {len(unmatched_data)}')
-
+        iteration_count = 0
+        for title1 in self.netflix_data['title']:
+            matches = process.extractBests(title1, unmatched_data['title'], scorer=fuzz.ratio, score_cutoff=90)
+            for match in matches:
+                title2 = match[0]
+                distance = fuzz.ratio(title1, title2)
+                if distance <= threshold:
+                    mismatched_titles.append((title1, title2, distance))
+            iteration_count += 1
+            if test_mode and iteration_count >= max_iterations:
+                break
+        return mismatched_titles
 
 
 if __name__ == "__main__":
@@ -22,7 +32,9 @@ if __name__ == "__main__":
     netflix_path = f'{project_root}/data/netflix_titles.csv'
     tmdb_path = f'{project_root}/data/TMDb_updated.CSV'
     n = Netflix(netflix_path, tmdb_path)
-       
+    for mismatch in n.mismatches:
+        print(mismatch)
+         
   
     
 
