6c5bbd171a1c44b61984522129ad84dd36eaf79f
Suhail <suhailece@gmail.com>
2024-04-28 17:51:58 -0700

    update scripts

--- pythonkitchen/textureRen/scripts/script2.py
+++ pythonkitchen/textureRen/scripts/script2.py
@@ -1,6 +1,7 @@
 import os
 import json
 import shutil
+import csv
 
 # Get the VFX_LIB environment variable
 ASSET_INGEST_DIR = os.getenv('ASSET_INGEST_DIR')
@@ -9,7 +10,20 @@ VFX_LIB = os.getenv('VFX_LIB')
 # Define the paths
 ingest_folder = os.path.join(ASSET_INGEST_DIR, 'textures', 'hdri')
 hdri_folder = os.path.join(VFX_LIB, 'hdri')
-renamed_json_path = os.path.join(os.path.dirname(__file__), 'textures_renamed.json')
+parent_dir = os.path.abspath(os.path.join(__file__,'../..'))
+renamed_json_path = os.path.join(parent_dir, 'textures_renamed.json')
+csv_file_path = './hdri_file_attributes.csv'
+hdri_json_path = '../hdri.json'
+
+
+#load CSV file
+def load_csv(file_path):
+    data = []
+    with open(file_path, newline='') as csvfile:
+        reader = csv.DictReader(csvfile)
+        for row in reader:
+            data.append(row)
+    return data
 
 
 # Load JSON files
@@ -17,53 +31,54 @@ def load_json(file_path):
     with open(file_path, 'r') as json_file:
         return json.load(json_file)
 
+
 # Rename files and update JSON
 def rename_files_and_update_json():
-    hdri_data = load_json('../hdri.json')
+    hdri_data = load_csv(csv_file_path)
+    json_data = load_json(hdri_json_path)
     naming_codes = load_json('../namingcodes.json')
-    renamed_hdri_data = []
+    renamed_json_data = load_json(renamed_json_path)
 
-    for old_filename, attributes in hdri_data['hdri'].items():
-        source = naming_codes['source'].get(attributes['source'], 'unknown')
-        scene = naming_codes['scene'].get(attributes['scene'], 'unknown')
-        clouds = naming_codes['clouds'].get(attributes['clouds'], 'unknown')
-        hour = naming_codes['hour'].get(attributes['hour'], 'unknown')
-        color = naming_codes['color'].get(attributes['color'], 'unknown')
-        variant = attributes['variant']
+    for entry in hdri_data:
+                
+        old_filename = entry['file']
+        old_file_path = entry['file_path']
+        source = naming_codes['source'][entry['source']]
+        scene = naming_codes['scene'][entry['scene']]
+        clouds = naming_codes['clouds'][entry['clouds']]
+        hour = naming_codes['hour'][entry['hour']]
+        resol = entry['resol']
+        color = naming_codes['color'][entry['color']]
+        variant = naming_codes['variant'][entry['variant']]
         file_extension = old_filename.split('.')[-1]
 
         # Generate new file name
         new_filename = f'{source}_{scene}_{clouds}_{hour}_{color}_{variant}.{file_extension}'
-
+        
+        # Check if entry already exists in renamed_json_data
+        if new_filename not in renamed_json_data['hdri']:
+            renamed_json_data['hdri'][new_filename] = {
+                'source': source,
+                'scene': scene,
+                'clouds': clouds,
+                'hour': hour,
+                'color': color,
+                'resol': resol,
+                'variant': variant,
+                'old_file_name': old_filename
+            }
         # Rename the file
-        old_file_path = attributes['file_path']
         new_file_path = os.path.join(hdri_folder, new_filename)
         shutil.move(old_file_path, new_file_path)
 
-        # Update JSON data
-        attributes['file_path'] = new_file_path
-        attributes['new_file_name'] = new_filename
-        renamed_hdri_data.append({
-            'new_file_name': new_filename,
-            'source': source,
-            'scene': scene,
-            'clouds': clouds,
-            'hour': hour,
-            'color': color,
-            'resol': attributes['resol'],
-            'variant': variant,
-            'old_file_name': old_filename
-        })
 
-    # Write the updated data back to the original file
     with open(renamed_json_path, 'w') as json_file:
-        json.dump(renamed_hdri_data, json_file, indent=4)
+        json.dump(renamed_json_data, json_file, indent=4)
 
-    print(f"Files renamed and JSON updated successfully. Renamed data saved to '{renamed_json_path}'.")
 
-# Main function
 def main():
     rename_files_and_update_json()
 
 if __name__ == "__main__":
     main()
+
